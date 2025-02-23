482a759a053a77c175d29db60a89a1346c5ea2ac
sheabali <iamskpranto@gmail.com>
2025-02-24 00:57:32 +0600

    working at goole login

--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -1,10 +1,15 @@
 import { NextAuthOptions } from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
