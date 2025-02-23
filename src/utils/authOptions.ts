995bf86f7dd1aa7ef2991f61baabf7b56af85f35
sheabali <iamskpranto@gmail.com>
2025-02-24 00:10:28 +0600

    implement GitHub authentication system

--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -1,8 +1,12 @@
 import { NextAuthOptions } from 'next-auth';
+import GitHubProvider from 'next-auth/providers/github';
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
-    // ...add more providers here
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
   ],
 };
